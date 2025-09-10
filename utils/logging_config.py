41b3d875f732f8b16b89947e2db079be2dd57082
Prince Raj <prince.raj@intel.com>
2025-09-10 08:55:59 +0300

    changing the logging info

--- utils/logging_config.py
+++ utils/logging_config.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 def setup_logging(log_level=logging.WARNING, project_namespace=None):
     """Setup global logging configuration."""
-
+    import pdb;pdb.set_trace()
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_file = f"vca_{timestamp}.log"
     Path('logs').mkdir(parents=True, exist_ok=True)
@@ -22,8 +22,8 @@ def setup_logging(log_level=logging.WARNING, project_namespace=None):
         logger = logging.getLogger(project_namespace) 
         logger.setLevel(log_level)
 
-    if logger.hasHandlers():
-        return logger 
+    if logger.handlers:
+        return logger
 
     log_format = logging.Formatter(
         "%(asctime)s [%(levelname)s] %(name)s - %(message)s",
@@ -48,3 +48,4 @@ def setup_logging(log_level=logging.WARNING, project_namespace=None):
     file_handler.setLevel(log_level)
     file_handler.setFormatter(log_format)
     logger.addHandler(file_handler)
+    return logger
